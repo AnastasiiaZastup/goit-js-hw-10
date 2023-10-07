e854fcdf6e0dba0d9ab6b462f6227843a7c05704
AnastasiaZastup <zastupanastasia7@gmail.com>
2023-10-07 19:40:41 +0300

    my commit

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import axios from "axios";
 axios.defaults.headers.common["x-api-key"] = "live_iSRIpIDGcz7xbDEvu0w2whwiemTunA6qYEKa8fPsjeRDNYIEsvN0R7IYBm3oCfAW";
 import { fetchBreeds, fetchCatByBreed } from './cat-api'
 
-const breedSelect = document.querySelector('.breed-select');
+ function createMarkup(catData, breedData){
+        return `<img src='${catData.url}' width='350' alt='${breedData.name}'/>
+        <div class='textInformation'><h1>${breedData.name}</h1>
+        <p>${breedData.description}</p>
+        <p><b>Temperament:</b> ${breedData.temperament}</p></div>`
+} 
 
-const infoLoader = document.querySelector('.loader');
+const breedSelect = document.querySelector('.breed-select');
+const informationLoader = document.querySelector('.loader');
 const selectError = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
 
-window.addEventListener('load', init);
+window.addEventListener('load', startInit);
 
-function init(){
+function startInit(){
     let breedsData;
     fetchBreeds()
     .then(data => {
@@ -22,22 +28,22 @@ function init(){
             option.textContent = breed.name;
             breedSelect.appendChild(option);
 
-            breedSelect.classList.remove('is-hidden');
-            infoLoader.classList.add('is-hidden');
+            breedSelect.classList.remove('hidden');
+            informationLoader.classList.add('hidden');
           })
     })
     .catch(error => {
         console.log(error);
-        selectError.classList.remove('is-hidden');
-        breedSelect.classList.add('is-hidden');
-        infoLoader.classList.add('is-hidden');
+        selectError.classList.remove('hidden');
+        breedSelect.classList.add('hidden');
+        informationLoader.classList.add('hidden');
     })
 
     breedSelect.addEventListener('change', () => {
         const selectBreedId = breedSelect.value;
-        infoLoader.classList.remove('is-hidden');
-        catInfo.classList.add('is-hidden');
-        selectError.classList.add('is-hidden');
+        informationLoader.classList.remove('hidden');
+        catInfo.classList.add('hidden');
+        selectError.classList.add('hidden');
 
         fetchCatByBreed(selectBreedId)
         .then(result => {
@@ -47,20 +53,42 @@ function init(){
             const markup = createMarkup(catData, breedData);
             catInfo.innerHTML = markup;
 
-            infoLoader.classList.add('is-hidden');
-            catInfo.classList.remove('is-hidden');
+            informationLoader.classList.add('hidden');
+            catInfo.classList.remove('hidden');
         })
         .catch(error => {
             console.log(error);
-            selectError.classList.remove('is-hidden');
-            infoLoader.classList.add('is-hidden');
+            selectError.classList.remove('hidden');
+            informationLoader.classList.add('hidden');
         })
     });
 }
 
-        function createMarkup(catData, breedData){
-        return `<img src='${catData.url}' width='300' alt='${breedData.name}'/>
-        <div class='textInfo'><h1>${breedData.name}</h1>
-        <p>${breedData.description}</p>
-        <p><b>Temperament:</b> ${breedData.temperament}</p></div>`
-    }
+  
+ /* fetchCatByBreed(selectedBreedId)
+    .then(function (result) {
+      const catData = result[0];
+      const breedData = breedsData.find(function (breed) {
+        return breed.id === catData.breeds[0].id;
+      });
+
+      const markup = createMarkup(catData, breedData);
+      catInfo.innerHTML = markup;
+      hideLoader();
+      displayCatInfo();
+    })
+    .catch(function (error) {
+      console.error(error);
+      showSelectError();
+      hideLoader();
+    });
+});*/
+
+
+
+
+
+
+
+
+    
